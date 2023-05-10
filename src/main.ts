a35081cef245d0a4889783f81ffc21a914ffaf43
loan <loan.courchinoux-billonnet@koesio.com>
2023-05-10 14:45:54 +0200

    configure base project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import configurationConfig from './config/configuration.config';
+
+const configService = new ConfigService(configurationConfig());
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(configService.get('port'));
 }
 bootstrap();
